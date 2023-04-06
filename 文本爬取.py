c0e45ad160a13c7b6aebdd88945d0384979bd6ea
marklovestudy <129322870+marklovestudy@users.noreply.github.com>
2023-04-06 16:51:05 +0800

    Delete 文本爬取.py

--- 文本爬取.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import urllib.parse
-url='https://www.baidu.com/'
-response=urllib.request.urlopen(url=url)
-response=response.read().decode('utf-8')
-print(response)
-with open('4K.text','w',encoding='utf-8') as f:
-    f.write(response)
